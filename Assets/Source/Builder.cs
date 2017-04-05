04cdcd329db329147cbfbb9e9348e102c462692a
Yemel Jardi <angel.jardi@gmail.com>
2017-04-04 21:28:28 -0300

    serialize message handler

--- Assets/Source/Builder.cs
+++ Assets/Source/Builder.cs
@@ -4,27 +4,62 @@ using UnityEngine;
 
 public class Builder : MonoBehaviour {
 
-	public GameObject builder;
-	public GameObject tile;
+	public GameObject tool;
+	public GameObject floor;
+	public GameObject parent;
+
+	public Material m1;
+	public Material m2;
+	public Material m3;
+
+	public static float CUBE_SIZE = 0.5f;
 
 	// Use this for initialization
 	void Start () {
-		
+		float cubes = floor.transform.localScale.x / CUBE_SIZE / 2;
+		for (float i = -cubes ; i < cubes; i++) {
+			for (float j = -cubes; j < cubes; j++) {
+				float x = i * CUBE_SIZE + 0.25f;
+				float z = j * CUBE_SIZE + 0.25f;
+				CreateCube (new Vector3 (x, -0.25f, z));
+			}
+		}
+	}
+
+
+	private GameObject CreateCube(Vector3 position) {
+		GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		go.transform.position = position;
+		go.transform.localScale = new Vector3 (CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
+		go.GetComponent<MeshRenderer> ().material = tool.GetComponent<MeshRenderer> ().material;
+		return go;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-			GameObject newObject = Object.Instantiate (builder);
-			newObject.transform.SetParent (this.transform);
-			newObject.transform.position = builder.transform.position;
+			GameObject cube = CreateCube (tool.transform.position);
+			cube.transform.parent = parent.transform;
 		}
 
-		if (Input.GetMouseButtonDown (1)) {
-			STile original = new STile (tile);
-			string content = original.ToBase64();
-			Debug.Log (content);
-			Application.ExternalEval("console.log('" + content + "')");
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, 4)) {
+			tool.transform.position = hit.transform.position + (hit.normal * CUBE_SIZE);
+			tool.transform.rotation = hit.transform.rotation;
+		} else {
+			tool.transform.position = new Vector3 (0, -10, 0);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			tool.GetComponent<MeshRenderer> ().material = m1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			tool.GetComponent<MeshRenderer> ().material = m2;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			tool.GetComponent<MeshRenderer> ().material = m3;
 		}
 
 	}
