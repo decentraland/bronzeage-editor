96e2b4625a9d9235894a0de3f7fd1abec357f2ee
Martín Triay <martriay@gmail.com>
2017-06-16 19:45:00 -0300

    [Builder] Hit publish HTTP endpoint

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -18,53 +18,36 @@ public class Control : MonoBehaviour {
 	private bool publishError = false;
 	private bool published = false;
 
-	public InputField input_url;
-	public InputField input_token;
-	public InputField input_x;
-	public InputField input_y;
+	private string api_url = "http://138.197.44.64:6748";
 
 	public void OnSubmit() {
 		GameObject tile = GameObject.Find("My Tile");
  		STile original = new STile (tile);
 
-		// Compute index
- 		int xOffset = int.Parse (input_x.text);
- 		int yOffset = int.Parse (input_y.text);
-
- 		Vector2 index = new Vector2(
- 			(tile.transform.position.x / TILE_SIZE) + xOffset,
- 			(tile.transform.position.z / TILE_SIZE) + yOffset
- 		);
-
 		// Serialize
  		string content = original.ToBase64();
-
- 		PublishTile(index, content);
-
+ 		PublishTile(content);
 		// TODO: Display submit feedback
 	}
 
-	void PublishTile(Vector2 index, string content) {
+	void PublishTile(string content) {
 		publishing = true;
 		publishError = false;
 		published = false;
 
 		// Basic Auth
 		Dictionary<string,string> headers = new Dictionary<string, string>();
-		headers["Authorization"] = "Basic " + System.Convert.ToBase64String(
-			System.Text.Encoding.ASCII.GetBytes("bitcoinrpc:" + input_token.text)
-		);
+		byte[] data = System.Text.Encoding.ASCII.GetBytes(content);
 
-		string json = "{\"method\":\"settile\",\"params\":[" + index [0] + "," + index [1] + ",\"" + content + "\"],\"id\":0}";
-		byte[] data = System.Text.Encoding.ASCII.GetBytes(json.ToCharArray());
-
-		WWW www = new WWW(input_url.text, data, headers);
+		print(content);
+		WWW www = new WWW(api_url, data, headers);
 
 		while (! www.isDone) {} // busy wait
 
 		if (string.IsNullOrEmpty(www.error)) {
 			RPCResponse response = JsonUtility.FromJson<RPCResponse>(www.text);
 			Debug.Log("Successfully published tile!");
+			Debug.Log(www.text);
 			published = true;
 		} else {
 			Debug.Log("Error publishing tile! " + www.error);
